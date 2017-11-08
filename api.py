f093a6d07d754c52cc2b4ba2172d74bd19180026
anapple07 <anapple07@users.noreply.github.com>
2017-11-08 20:53:06 +0900

    connect database

--- api.py
+++ api.py
@@ -3,13 +3,26 @@
 
 import json
 from functools import wraps
-
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, jsonify, request, url_for, abort, Response
 
 app = Flask(__name__)
+#TODO:testをcloudに
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/test?charset=utf8'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+#TODO:test_dataをinstance_listに
+class test_data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    instance_name= db.Column(db.String(80), unique=True)
+
+    def __init__(self, instance_name):
+        self.instance_name = instance_name
 
 @app.route('/', methods=['GET'])
 def home():
+    instance_list = test_data.query.all()
     return 'hello:)'
 
 @app.route('/api/<string:instance_id>', methods=['POST'])
