2350e0253e3d2231af9dd85aa17bd29f04474505
Sone_Liu <liujia4203@icloud.com>
2017-11-21 17:52:00 +0900

    Add files via upload

---
+++ dcm/catch_vm.py
@@ -0,0 +1,46 @@
+import os
+import pymysql as mysql
+import xml.etree.ElementTree as ET
+
+db = mysql.connect(user='root',passwd='Root%123',db='cloud',host='kvmhost01')
+db.autocommit(True)
+cursor = db.cursor()
+
+cursor.execute("select hostname from addvm_list")
+results= cursor.fetchall()
+for row in results:
+    vmname = row[0]
+
+tree = ET.parse('/var/kvm/%s.xml' %vmname)
+root = tree.getroot()
+global ins_uuid,ins_name,ip_addr,host_id,ram,vcpus,status
+ins_uuid=root.find('uuid').text
+ins_name=root.find('name').text
+ram=root.find('memory').text
+vcpus=root.find('vcpu').text
+mac_addr=root.find('devices/interface/mac')
+mac_addr=mac_addr.get('address')
+searchip=os.popen('ip n |grep -i %s' %mac_addr).read()
+ip_addr=searchip[0:searchip.rfind('dev',1)-1]
+host_id=4
+status=os.popen('virsh domstate %s' %ins_name).read()
+
+sql="insert into instance_list(uuid,name,ip_addr,host_id,ram,vcpus,status) values('%s','%s','%s','%s','%s','%s','%s')"%(ins_uuid,ins_name,ip_addr,host_id,ram,vcpus,status)
+cursor.execute(sql)
+cleardata="TRUNCATE TABLE addvm_list"
+cursor.execute(cleardata)
+db.close()
+print("VM data input")
+#print ins_uuid,ins_name,ram,vcpus,ip_addr,host_id,status
+
+#root.find('').text
+
+#disk=root.find('devices/disk/source')
+#disk=disk.get('file')
+
+#host_id=os.popen('hostname').read()
+#vm_id=os.popen('virsh domid %s' %ins_name).read()
+
+
+#print ins_uuid,ins_name,ram,disk,vcpus,os_type,os_variant,network_bridge,ip_addr,host_id,status
+#sql="INSERT INTO instance_list(ins_uuid,ins_name,ram,disk,vcpus) VALUES('%s','%s','%s','%s','%s')" %(ins_uuid,ins_name,ram,disk,vcpus)
---
+++ dcm/dcm.py
@@ -0,0 +1,63 @@
+import os
+import sys
+import pymysql as mysql
+import time
+###catch addvm data#
+add_name=raw_input("Input the name of VM:")
+add_memory=raw_input("Input the size of memory:")
+add_disk=raw_input("Input the size of disk:")
+add_ip=raw_input("Input the hope of IP Address(Emtry is ok):")
+add_admin=raw_input("Input the admin of VM:")
+
+
+###updata the free disk###
+os.system("ssh root@192.168.1.3 -i key2 'python catch_host.py' ")
+time.sleep(2)
+os.system("ssh root@192.168.1.4 -i key3 'python catch_host.py' ")
+time.sleep(2)
+os.system("ssh root@192.168.1.5 -i key4 'python catch_host.py' ")
+###updata vmname###
+
+###check vmname###
+db = mysql.connect(user='root',passwd='Root%123',db='cloud',host='kvmhost01')
+db.autocommit(True)
+cursor = db.cursor()
+checkname="SELECT * FROM instance_list WHERE name='%s'" %add_name
+cursor.execute(checkname)
+check = cursor.fetchall()
+'''
+if check:
+    print "Error: An VM of the same name already exists."
+else:
+    print "hostname:'%s' is ok!" %add_name
+'''
+###manager###
+
+checkdisk = "SELECT host_name,total FROM host_list WHERE total >'%s' ORDER BY RAND() LIMIT 1" %add_disk
+cursor.execute(checkdisk)
+results = cursor.fetchall()
+for row in results:
+    server_name = row[0]
+    free_disk = row[1]
+db.close()
+###write data to cloud.addvm##
+db = mysql.connect(user='root',passwd='Root%123',db='cloud',host='kvmhost01')
+db.autocommit(True)
+cursor = db.cursor()
+writeadd="INSERT INTO addvm_list(hostname,memory,disk,network,admin) VALUES('%s',%s,%s,'%s','%s')" %(add_name,add_memory,add_disk,add_ip,add_admin)
+cursor.execute(writeadd)
+
+###ADD VM###
+print('The new VM in %s' %server_name)
+time.sleep(2)
+keynumber=int(server_name[-1])
+addcmd="ssh root@%s -i key%s 'python createvm_xml.py'" %(server_name,keynumber)
+os.system(addcmd)
+new_disk=int(float(free_disk)-float(add_disk))
+updatedisk="UPDATE host_list SET total='%s' WHERE host_name='%s' " %(new_disk,server_name)
+try:
+        cursor.execute(updatedisk)
+except:
+        print "Error: unable to fecth data"
+db.close()
+print('The disk of %s updated.' %server_name)
